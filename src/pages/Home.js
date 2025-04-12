a50588529a2f47b9d15a8e38e34c29be8f57d91a
lalit8694 <lalit8694123@gmail.com>
2025-04-13 00:39:42 +0530

    update file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,6 +14,7 @@ useEffect(()=>{
 },[]);
 
 const loadUsers= async ()=>{
+  console.log(`Here : ${process.env.REACT_APP_API_URL}`)
   const result = await axios.get("http://localhost:8080/students")
   console.log(result.data)
   setUsers(result.data)
